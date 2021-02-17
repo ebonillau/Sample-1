4b60e3885feaefb10bef39c2bef8fc34cf150724
enriquebonilla <nexen.aplicaciones@gmail.com>
2021-02-17 06:19:10 -0500

    Initial Commit

---
+++ CleanSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSample
+//
+//  Created by Enrique Bonilla on 17/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
